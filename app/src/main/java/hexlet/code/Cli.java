c3f8c1745e21c13a92164dc4304951c095de841d
Ivan Zubarev <swim_zb@mail.ru>
2025-02-05 00:23:11 +0700

    fix Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,6 +3,6 @@ package hexlet.code;
 public class Cli {
     public static void greeting(String userName) {
 
-        System.out.print("Hello, " + userName);
+        System.out.print("Hello, " + userName + "!");
     }
 }
