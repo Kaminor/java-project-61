c2833b59826e9caeddc3eac1937d87bd115fca23
Ivan Zubarev <swim_zb@mail.ru>
2025-02-09 13:14:16 +0700

    update GCD

--- app/src/main/java/hexlet/code/GCD.java
+++ app/src/main/java/hexlet/code/GCD.java
@@ -3,9 +3,25 @@ import  java.util.Scanner;
 import  java.util.Random;
 
 public class GCD {
-    public static void findGCD() {
+    static final int greatestNumberMaxValue = 300;
+    static final int smallestNumberMaxValue = 20;
+    static Random random = new Random();
+
+
+    static int calculateGCD(int randomNumberOne, int randomNumberTwo) {
+        int gcdNumber = 0;
+
+        for (var j = 1; j <= Math.max(randomNumberOne, randomNumberTwo); j++) {
+            if (randomNumberOne % j == 0 && randomNumberTwo % j == 0) {
+                gcdNumber = j;
+            }
+        }
+        return gcdNumber;
+    }
+
+
+    public static void startPlay() {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
         String greetingMessage = "Welcome to the Brain Games!";
 
         System.out.println(greetingMessage);
@@ -17,30 +33,22 @@ public class GCD {
 
         int countOfWinners = 0;
         final int attempts = 3;
-        final int greatestNumberMaxValue = 300;
-        final int smallestNumberMaxValue = 20;
 
         for (var i = 0; i < attempts; i++) {
             int randomNumberOne = random.nextInt(greatestNumberMaxValue) + 1;
             int randomNumberTwo = random.nextInt(smallestNumberMaxValue) + 1;
-            int gcdNumber = 0;
-
-            for (var j = 1; j <= Math.max(randomNumberOne, randomNumberTwo) / 2; j++) {
-                if (randomNumberOne % j == 0 && randomNumberTwo % j == 0) {
-                    gcdNumber = j;
-                }
-            }
+            calculateGCD(randomNumberOne, randomNumberTwo);
 
             System.out.println("Question: " + randomNumberOne + " " + randomNumberTwo);
             System.out.println("Your answer: ");
 
             String calcResultAnswer = scanner.next();
 
-            if (String.valueOf(gcdNumber).equals(calcResultAnswer)) {
+            if (String.valueOf(calculateGCD(randomNumberOne, randomNumberTwo)).equals(calcResultAnswer)) {
                 System.out.println("Correct!");
                 countOfWinners++;
             } else {
-                Engine.showFail(calcResultAnswer, gcdNumber);
+                Engine.showFail(calcResultAnswer, calculateGCD(randomNumberOne, randomNumberTwo));
                 System.out.println("Let's try again, " + userName + "!");
                 break;
             }
