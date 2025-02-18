65b1089b6d978ef5edec0796e56d1fa587a9cdf2
Ivan Zubarev <swim_zb@mail.ru>
2025-02-18 23:12:12 +0700

    update Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,13 +1,27 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class Cli {
-    public static void greetUser(String userName) {
+    public static String chooseGame() {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Hello, " + userName + "!");
+        App.showChoice();
+        String choice = scanner.next();
+        System.out.println("Your choice: " + choice);
+        return choice;
     }
 
     public static void greet() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name? ");
+
+        String userName = scanner.next();
+        System.out.print("Hello, " + userName + "!");
+    }
+
+    public static void greetUser(String userName) {
+        System.out.print("Hello, " + userName + "!");
     }
 }
